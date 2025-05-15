9b074bbb410cf259a81ba40a7574abc31c3b541b
David Espin <jespin0316@utm.edu.ec>
2025-05-15 15:42:56 -0500

    Corrección datos division

--- Calculadora.js
+++ Calculadora.js
@@ -9,6 +9,7 @@ class Calculadora{
         return num1*num2;
     }
         dividir(num1,num2){
-        return num1/num2;
+       if(num2 === 0) return "no se puede dividir para cero"; 
+       return num1/num2;
     }
 }
