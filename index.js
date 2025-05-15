c727669b33530027cd0e4fb8ec6816b81a8a4ffc
David Espin <jespin0316@utm.edu.ec>
2025-05-15 11:52:55 -0500

    Creación de la Clase Display

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const displayValorAnterior = document.getElementById('valor_anterior');
 const displayValorActual = document.getElementById('valor_actual');
 
+const display = new Display(displayValorAnterior, displayValorActual);
