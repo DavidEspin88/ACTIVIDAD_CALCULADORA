7eba5e043d280f6f42f2d5b28cdc66fd0def0b8e
David Espin <jespin0316@utm.edu.ec>
2025-05-15 15:18:15 -0500

    Configuración de teclado para ingresar valores

--- Display.js
+++ Display.js
@@ -1,10 +1,27 @@
 class Display{
     constructor(displayValorAnterior, displayValorActual){
-        this.displayValorActual= displayValorActual;
-        this.displayValorAnterior= displayValorAnterior;
+        this.displayValorActual=displayValorActual;
+        this.displayValorAnterior=displayValorAnterior;
+        this.tipOperacion=undefined;
         this.calculador = new Calculadora();
         this.valorActual = '';
-        this.valorAnterior = '';    
+        this.valorAnterior='';
+        this.signos ={
+            sumar:'+',
+            restar:'-',
+            dividir:'/',
+            multiplicar:'x',
+        }
     }
+ 
+    agregarNumero(numero){
+        this.valorActual= this.valorActual.toString()+numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+    this.displayValorActual.textContent = this.valorActual;
+    this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipOperacion] || ''}`;
+}
 
 }
--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const displayValorAnterior = document.getElementById('valor_anterior');
 const displayValorActual = document.getElementById('valor_actual');
+const botonesNumeros= document.querySelectorAll('.numero');
+const botonesOperadores= document.querySelectorAll('.operador');
 
-const display = new Display(displayValorAnterior, displayValorActual);
+const display = new Display(displayValorAnterior,displayValorActual);
+
+document.addEventListener('keydown', event => {
+    const tecla = event.key;
+
+    // Si presiona números o punto
+    if (!isNaN(tecla) || tecla === '.') {
+        display.agregarNumero(tecla);
+    }
+
+});
